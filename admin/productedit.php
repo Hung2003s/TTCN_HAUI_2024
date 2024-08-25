e95d5873d49c07ba446429f5c67d4ee710c60edb
Hung2003s <130260902+Hung2003s@users.noreply.github.com>
2024-08-26 02:52:58 +0700

    xoa va tehm file

--- admin/productedit.php
+++ admin/productedit.php
@@ -8,24 +8,29 @@ include "leftside.php";
 ?>
 <?php
 $product = new product();
+if (isset($_GET['sanpham_id'])|| $_GET['sanpham_id']!=NULL){
+    $sanpham_id = $_GET['sanpham_id'];
+    }
+    $get_sanpham = $product -> get_sanpham($sanpham_id);
+    if($get_sanpham){$resul = $get_sanpham ->fetch_assoc();}
+  
+?>
+<?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])){
-    // var_dump($_POST);
-	$insert_product = $product ->insert_product($_POST,$_FILES);
-    header('Location:brandlist.php');
-
+	$update_product = $product ->update_product($_POST,$_FILES,$sanpham_id );
+    // header('Location:brandlist.php');
 }
-
 ?>
  <div class="admin-content-right">
             <div class="product-add-content">
                 <?php
                 if(isset($insert_product)){echo $insert_product; }
                 ?>
-                <form action="productadd.php" method="POST" enctype="multipart/form-data">
+                <form action="" method="POST" enctype="multipart/form-data">
                     <label for="">Tên sản phẩm<span style="color: red;">*</span></label> <br>
-                    <input required type="text" name="sanpham_tieude"> <br>
-                    <label for="">Mã sản phẩm<span style="color: red;">*</span></label> <br>
-                    <input required type="text" name="sanpham_ma"> <br>
+                    <input value="<?php echo $resul['sanpham_tieude'] ?>" required type="text" name="sanpham_tieude"> <br>
+                    <label  for="">Mã sản phẩm<span style="color: red;">*</span></label> <br>
+                    <input value="<?php echo $resul['sanpham_ma'] ?>" required type="text" name="sanpham_ma"> <br>
                     <!-- <label for="">Ngày bài viết<span style="color: red;">*</span></label> <br>
                     <input type="text" name="baiviet_ngay"> <br>
                     <label for="">Tác giả bài viết<span style="color: red;">*</span></label> <br>
@@ -37,14 +42,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])){
                         $show_danhmuc = $product ->show_danhmuc();
                         if($show_danhmuc){while($result=$show_danhmuc->fetch_assoc()){
                         ?>
-                        <option value="<?php echo $result['danhmuc_id'] ?>"><?php echo $result['danhmuc_ten'] ?></option>
+                        <option <?php if($resul['danhmuc_id']== $result['danhmuc_id']) {echo "selected";} ?> value="<?php echo $result['danhmuc_id'] ?>"><?php echo $result['danhmuc_ten'] ?></option>
                         <?php
                         }}
                         ?>
                     </select>
                     <label for="">Chọn Loại sản phẩm<span style="color: red;">*</span></label> <br>
                     <select required="required" name="loaisanpham_id" id="loaisanpham_id">
-                        <option value="">--Chọn--</option>
+                         <option value="">--Chọn--</option>
+                        <?php
+                        $show_loaisanpham = $product ->show_loaisanpham();
+                        if($show_loaisanpham){while($result=$show_loaisanpham->fetch_assoc()){
+                        ?>
+                        <option <?php if($resul['loaisanpham_id']== $result['loaisanpham_id']) {echo "selected";} ?> value="<?php echo $result['loaisanpham_id'] ?>"><?php echo $result['loaisanpham_ten'] ?></option>
+                        <?php
+                        }}
+                        ?>
                       
                     </select>
                     <label for="">Chọn Màu sản phẩm<span style="color: red;">*</span></label> <br>
@@ -54,29 +67,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])){
                         $show_color = $product ->show_color();
                         if($show_color){while($result=$show_color->fetch_assoc()){
                         ?>
-                        <option value="<?php echo $result['color_id'] ?>"><?php echo $result['color_ten'] ?></option>
+                        <option  <?php if($resul['color_id']== $result['color_id']) {echo "selected";} ?> value="<?php echo $result['color_id'] ?>"><?php echo $result['color_ten'] ?></option>
                         <?php
                         }}
                         ?>
                     </select>
-                    <label for="">Chọn Size sản phẩm<span style="color: red;">*</span></label> <br>
-                    <div class="sanpham-size">
-                    <p>S</p>    <input type="checkbox" name="sanpham-size[]" value="S"> 
-                    <p>M</p>    <input type="checkbox" name="sanpham-size[]" value="M"> 
-                    <p>L</p>    <input type="checkbox" name="sanpham-size[]" value="L">
-                    <p>XL</p>   <input type="checkbox" name="sanpham-size[]" value="XL">  
-                    <p>XXL</p>  <input type="checkbox" name="sanpham-size[]" value="XXL">  
-                    </div>
                     <label for="">Giá sản phẩm<span style="color: red;">*</span></label> <br>
-                    <input required type="text" name="sanpham_gia"> <br>  
+                    <input value="<?php echo $resul['sanpham_gia'] ?>" required type="text" name="sanpham_gia"> <br>  
                     <label for="">Chi tiết<span style="color: red;">*</span></label> <br>
-                    <textarea class="ckeditor"  required name="sanpham_chitiet" cols="60" rows="5"></textarea><br>  
+                    <textarea class="ckeditor"  required name="sanpham_chitiet" cols="60" rows="5"><?php echo $resul['sanpham_chitiet'] ?></textarea><br>  
                     <label  for="">Bảo quản<span style="color: red;">*</span></label> <br>
-                    <textarea class="ckeditor" required name="sanpham_baoquan" cols="60" rows="5"></textarea><br> 
+                    <textarea class="ckeditor" required name="sanpham_baoquan" cols="60" rows="5"><?php echo $resul['sanpham_baoquan'] ?></textarea><br> 
                     <label for="">Ảnh đại diện<span style="color: red;">*</span></label> <br>
-                    <input required type="file" name="sanpham_anh"> <br>   
+                    <img style="width: 100px; height: 100px" src="uploads/<?php echo $resul['sanpham_anh'] ?>"> <br> 
+                    <input  type="file" name="sanpham_anh"> <br>   
                     <label for="">Ảnh Sản phẩm<span style="color: red;">*</span></label> <br>
-                    <input required type="file" multiple  name="sanpham_anhs[]"> <br>   
+                    <div class="sanpham-anh">
+                         <?php
+                          $get_anh = $product -> get_anh($sanpham_id);                        
+                          if($get_anh){while($resultA=$get_anh->fetch_assoc()){
+                        ?>
+                         <img style="width: 100px; height: 100px" src="uploads/<?php echo $resultA['sanpham_anh'] ?>">
+                         <?php
+                        }}
+                        ?>
+                    </div>
+                    <input type="file" multiple  name="sanpham_anhs[]"> <br>   
                     <button class="admin-btn" name="submit" type="submit">Gửi</button>  
                 </form>
             </div>           

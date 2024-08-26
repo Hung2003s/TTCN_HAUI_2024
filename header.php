b2b10d49f12f19a4c3de89daf3d17926091182b0
vnst_64 <nbv2k3@gmail.com>
2024-08-26 17:21:39 +0700

    sửa tên file css

--- header.php
+++ header.php
@@ -22,8 +22,8 @@ $index = new index;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/54f0cb7e4a.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <link rel="stylesheet" href="css/mainstyle.css">
-    <title>Website - Ivy</title>
+    <link rel="stylesheet" href="stylefront.css">
+    <title>Website - VN Clothes</title>
 </head>
 <body>
     <secsion class="top">

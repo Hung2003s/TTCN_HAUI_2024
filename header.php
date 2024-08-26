c176a4b51b63d2554825a4a676feaeb1720f2e14
Hung2003s <130260902+Hung2003s@users.noreply.github.com>
2024-08-27 00:23:02 +0700

    ket noi js front end

--- header.php
+++ header.php
@@ -22,7 +22,7 @@ $index = new index;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/54f0cb7e4a.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <link rel="stylesheet" href="css/mainstyle.css">
+    <link rel="stylesheet" href="frontend/css/mainstyle.css">
     <title>Website - VN CLOTHES</title>
 </head>
 <body>
